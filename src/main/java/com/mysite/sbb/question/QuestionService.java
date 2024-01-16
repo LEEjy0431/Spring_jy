0795b6b533b99784e00a888d336a70911db2b87a
leejy <jys0431@gmail.com>
2024-01-16 10:35:31 +0900

    v3-4

--- src/main/java/com/mysite/sbb/question/QuestionService.java
+++ src/main/java/com/mysite/sbb/question/QuestionService.java
@@ -5,12 +5,15 @@ import java.util.ArrayList;
 import java.util.List;
 
 import com.mysite.sbb.DataNotFoundException;
+import com.mysite.sbb.answer.Answer;
 import com.mysite.sbb.user.SiteUser;
+import jakarta.persistence.criteria.*;
 import org.springframework.cglib.core.Local;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.stereotype.Service;
 
 import lombok.RequiredArgsConstructor;
@@ -23,6 +26,24 @@ import java.util.Optional;
 public class QuestionService {
 
     private final QuestionRepository questionRepository;
+
+    private Specification<Question> search(String kw){
+        return new Specification<Question>() {
+            private static final long serialVersionUID=1L;
+            @Override
+            public Predicate toPredicate(Root<Question> q, CriteriaQuery<?> query, CriteriaBuilder cb) {
+                query.distinct(true);
+                Join<Question, SiteUser> u1 =q.join("author", JoinType.LEFT);
+                Join<Question, Answer> a =q.join("answerList", JoinType.LEFT);
+                Join<Question, SiteUser> u2 =q.join("author", JoinType.LEFT);
+                return cb.or(cb.like(q.get("subject"), "%"+kw+"%"),
+                        cb.like(q.get("content"), "%"+kw+"%"),
+                        cb.like(u1.get("username"), "%"+kw+"%"),
+                        cb.like(a.get("content"), "%"+kw+"%"),
+                        cb.like(u2.get("username"), "%"+kw+"%"));
+            }
+        };
+    }
 
     public List<Question> getList() {
         return this.questionRepository.findAll();
@@ -43,11 +64,12 @@ public class QuestionService {
         q.setAuthor(user);
         this.questionRepository.save(q);
     }
-    public Page<Question> getList(int page){
+    public Page<Question> getList(int page, String kw){
         List<Sort.Order> sorts = new ArrayList<>();
         sorts.add(Sort.Order.desc("createData"));
         Pageable pageable= PageRequest.of(page,10);
-        return this.questionRepository.findAll(pageable);
+        Specification<Question> spec = search(kw);
+        return this.questionRepository.findAllByKeyword(kw, pageable);
     }
     public void modify(Question question, String subject, String content){
         question.setSubject(subject);
